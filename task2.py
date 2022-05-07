6536484e6d072888d1c49bc17514244f6664e1ac
Nazar Klypych <xxxnazarkoxxx@gmail.com>
2022-05-07 18:34:48 +0300

    Cleaning this branch fro homework files

--- task2.py
+++
@@ -1,9 +0,0 @@
-print('Hello world!')
-
-print(1, 2, 3, 4, 5, sep='\t')
-
-print(True, end='\n')
-
-print('Hi!', 'Nice', 'to', 'see', 'you', sep=' ')
-
-print('Never gonna give you up', end='!')
--- task3.py
+++
@@ -1,7 +0,0 @@
-# printing "O"
-print('#' * 9, '#\t\t#', '#\t\t#', '#\t\t#', '#' * 9, sep='\n')
-
-print('')
-
-# printing "H"
-print('#\t\t#', '#\t\t#', '#'*9, '#\t\t#', '#\t\t#', sep='\n')
